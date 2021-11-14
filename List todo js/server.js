9b1cd839488ef3c8d80551e65d09d96c04a6af96
Mohammad Ebad <91406268+ebad5214@users.noreply.github.com>
2021-11-14 22:23:31 +0530

    Add files via upload

---
+++ List todo js/server.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const server=express();
+const port=8080;
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+server.get('/',(req,resp)=>{
+    resp.setHeader("Content-Type","text/html")
+    resp.sendFile(__dirname+'/index.html');
+});
+server.get('/style.css',(req,resp)=>{
+    resp.sendFile(__dirname+'/style.css');
+});
+server.get('/app.js',(req,resp)=>{
+    resp.sendFile(__dirname+'/app.js');
+});
+server.listen(port,()=>{
+    console.log(`http://localhost:${port} started `);
+});  
